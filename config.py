541c088c5a1b6d0b954b4a900e243cbce40896c9
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-02 21:22:02 +0530

    Updated config

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
     # get a token from @BotFather
-    TOKEN = os.environ.get("TOKEN", "")
+    TOKEN = os.environ.get("TokenBot", "")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
+    APP_ID = int(os.environ.get("ID", 12345))
+    API_HASH = os.environ.get("HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
