1c97cbbaba1d7737dc63dcc496d04a82b96adff8
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-02 20:46:10 +0530

    Initial commit

---
+++ plugins/start.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Anand PS Kerala
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+from config import Config
+from translation import Translation
+
+import pyrogram
+from database import TRChatBase
