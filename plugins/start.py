2fdee28983c24e2086b3f870209e324fe9bfb969
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-09 21:02:38 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -20,14 +20,14 @@ import asyncio
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
 async def start(bot, update):
-await bot.send_message(chat_id=update.chat.id, text=Translation.START, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Commands", callback_data="commands"), InlineKeyboardButton("Info", url="https://t.me/keralasbots")]]))
+    await bot.send_message(chat_id=update.chat.id, text=Translation.START, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Commands", callback_data="commands"), InlineKeyboardButton("Info", url="https://t.me/keralasbots")]]))
 
 @pyrogram.Client.on_callback_query(pyrogram.Filters.callback_data("start"))
 async def start_back(bot, update):
-await bot.edit_message_text(chat_id=update.chat.id, text=Translation.START, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Commands", callback_data="commands"), InlineKeyboardButton("Info", url="https://t.me/keralasbots")]]))
+    await bot.edit_message_text(chat_id=update.chat.id, text=Translation.START, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Commands", callback_data="commands"), InlineKeyboardButton("Info", url="https://t.me/keralasbots")]]))
 
 
 @pyrogram.Client.on_callback_query(pyrogram.Filters.callback_data("commands"))
 async def commands(bot, update):
-await bot.edit_message_text(chat_id=update.chat.id, text=Translation.COMMAND, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Private Commands", url="https://t.me/keralasbots"), InlineKeyboardButton("Admin Commands", url="https://docs.pyrogram.org"),], [InlineKeyboardButton("Back", callback_data="start")]]))
+    await bot.edit_message_text(chat_id=update.chat.id, text=Translation.COMMAND, parse_mode="html", disable_web_page_preview=True, reply_to_message_id=update.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Private Commands", url="https://t.me/keralasbots"), InlineKeyboardButton("Admin Commands", url="https://docs.pyrogram.org"),], [InlineKeyboardButton("Back", callback_data="start")]]))
 
