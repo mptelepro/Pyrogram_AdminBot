5085664319c0c97a1b05e81acac5033391abb985
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-22 19:47:19 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,4 +7,4 @@ async def ping(bot, update):
     first = datetime.now()
     sent = await bot.send_message(chat_id=update.chat.id, text="<B>Pong!</B>", parse_mode="html")
     second = datetime.now()
-    await sent.edit_message_text(f"**Pong!** `{(second - first).microseconds / 1000}`ms")
+    await sent.edit_message_text(chat_id=update.message.chat.id, text=f"**Pong!** `{(second - first).microseconds / 1000}`ms")
