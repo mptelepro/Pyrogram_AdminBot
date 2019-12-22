12013e95a821a6a7c13be78538939bd1f88778f0
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-22 19:43:42 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import pyrogram
 
-@pyrogram.Client.on_message(Filters.command(["ping"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["ping"]))
 async def ping(bot, update):
     first = datetime.now()
     sent = await bot.send_message(chat_id=update.chat.id, text="<B>Pong!</B>", parse_mode="html")
