8cf27ca24eabf1b89215af6c71565b05630d80dc
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-23 12:08:47 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -13,5 +13,14 @@ async def setchat_title(bot, update):
 async def pin_message(bot, update):
     await bot.pin_chat_message(chat_id=update.chat.id, message_id=update.reply_to_message.message_id)
 
+@pyrogram.Client.on_message(pyrogram.Filters.command(["unpin"]) & pyrogram.Filters.group)
+async def unpin_message(bot, update):
+    await bot.unpin_chat_message(chat_id=update.chat.id)
 
 
+@pyrogram.Client.on_message(pyrogram.Filters.command(["ban"]) & pyrogram.Filters.group)
+async def ban(bot, update):
+    user_id = update.text[5:]
+    if update.reply_to_message is not None:
+        user_id = update.reply_to_message.user.id
+    await bot.kick_chat_member(chat_id=update.chat.id, user_id=user_id)
