74fb853a16d180fe870340de3de07cfd93d9e146
Anand P S <49161738+anandpskerala@users.noreply.github.com>
2019-12-23 14:45:06 +0530

    Update jsondump.py

--- plugins/jsondump.py
+++ plugins/jsondump.py
@@ -7,7 +7,7 @@ import pyrogram
 @pyrogram.Client.on_message(pyrogram.Filters.command(["jsondump"]))
 async def jsondump(bot, update):
     if len(str(update)) < 3000 and "-f" not in update.command:
-        await bot.send_message(chat_id=update.chat.id, text="<code>" + update + "</code>, parse_mode="HTML")
+        await bot.send_message(chat_id=update.chat.id, text="<code>Json Dump\n\n" + update + "\n\n@Keralasbots</code>, parse_mode="HTML")
     else:
         fname = f"dump-{update.chat.id}.json"
         with open(fname, "w") as f:
