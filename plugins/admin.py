cb692452d0108dee72005d9ff6270e9fc714cb69
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-28 14:01:47 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,7 +1,8 @@
 import pyrogram
 from config import Config
+from pyrogram.errors.RPCError
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["setitle"]) & pyrogram.Filters.group)
+@pyrogram.Client.on_message(pyrogram.Filters.command(["setitle", "setitle@edu_jokerbot", "setitle@Edu_jokerbot"]) & pyrogram.Filters.group)
 async def setchat_title(bot, update):
     if update.text.lower() == "/setitle@" + Config.bot_name:
         num = int(Config.netnum) + int(9)
@@ -27,6 +28,14 @@ async def ban(bot, update):
     if update.reply_to_message is None:
         command, user = update.text.split(" ", 2)
         user_id = int(user)
+        try:
+            b = await bot.get_chat_member(update.chat.id, user_id)
+        except RPCError:
+            await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning")
+            return
+        ban_text = "Another bit of dust\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>".format(update.from_user.id, update.from_user.first_name, b.user.id, b.user.first_name) 
     else:
         user_id = update.reply_to_message.from_user.id
+        ban_text = "Another bit of dust\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>".format(update.from_user.id, update.from_user.first_name, update.reply_to_message.from_user.id, update.reply_to_message.from_user.first_name) 
     await bot.kick_chat_member(chat_id=update.chat.id, user_id=user_id)
+    await bot.send_message(chat_id=update.chat.id, ban_text)
