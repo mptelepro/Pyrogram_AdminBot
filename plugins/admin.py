9be3ed0dfa0cd28951739fb4d8dba0bf002cef0d
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-29 13:20:51 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -62,14 +62,14 @@ async def ban(bot, update):
             try:
                 b = await bot.get_chat_member(update.chat.id, user_id)
             except RPCError:
-                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning")
+                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning", reply_to_message_id=update.message_id)
                 return
             ban_text = "Another bit of dust!\n\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>!".format(update.from_user.id, update.from_user.first_name, b.user.id, b.user.first_name) 
         else:
             user_id = update.reply_to_message.from_user.id
             ban_text = "Another bit of dust!\n\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>!".format(update.from_user.id, update.from_user.first_name, update.reply_to_message.from_user.id, update.reply_to_message.from_user.first_name) 
         await bot.kick_chat_member(chat_id=update.chat.id, user_id=user_id)
-        await bot.send_message(chat_id=update.chat.id, text=ban_text)
+        await bot.send_message(chat_id=update.chat.id, text=ban_text, reply_to_message_id=update.message_id)
     else:
         await bot.send_message(chat_id=update.chat.id, text="Who are you Non-Admin to command me?", reply_to_message_id=update.message_id)
 
@@ -89,7 +89,7 @@ async def tban(bot, update):
             try:
                 b = await bot.get_chat_member(update.chat.id, user_id)
             except RPCError:
-                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning")
+                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning", reply_to_message_id=update.message_id)
                 return
             ban_text = "Another bit of dust!\n\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>! for {}".format(update.from_user.id, update.from_user.first_name, b.user.id, b.user.first_name, timev) 
         else:
@@ -98,7 +98,7 @@ async def tban(bot, update):
             utime = await sec(timev)
             ban_text = "Another bit of dust!\n\n<a href='tg://user?id={}'>{}</a> Banned <a href='tg://user?id={}'>{}</a>! for {}".format(update.from_user.id, update.from_user.first_name, update.reply_to_message.from_user.id, update.reply_to_message.from_user.first_name, timev) 
         await bot.kick_chat_member(chat_id=update.chat.id, user_id=user_id, until_date=int(time.time()) + int(utime))
-        await bot.send_message(chat_id=update.chat.id, text=ban_text)
+        await bot.send_message(chat_id=update.chat.id, text=ban_text, reply_to_message_id=update.message_id)
     else:
         await bot.send_message(chat_id=update.chat.id, text="Who are you Non-Admin to command me?", reply_to_message_id=update.message_id)
 
@@ -117,7 +117,7 @@ async def unban(bot, update):
             try:
                 b = await bot.get_users(user_id)
             except RPCError:
-                await bot.send_message(chat_id=update.chat.id, text="Invalid username or user id")
+                await bot.send_message(chat_id=update.chat.id, text="Invalid username or user id", reply_to_message_id=update.message_id)
                 return
             unban_text = "Ok I will give a second chance for <a href='tg://user?id={}'>{}</a> !".format(b.id, b.first_name) 
         else:
@@ -126,9 +126,9 @@ async def unban(bot, update):
         try:
             await bot.unban_chat_member(chat_id=update.chat.id, user_id=user_id)
         except RPCError:
-            await bot.send_message(chat_id=update.chat.id, text="Invalid username or user id")
+            await bot.send_message(chat_id=update.chat.id, text="Invalid username or user id", reply_to_message_id=update.message_id)
             return
-        await bot.send_message(chat_id=update.chat.id, text=unban_text)
+        await bot.send_message(chat_id=update.chat.id, text=unban_text, reply_to_message_id=update.message_id)
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["mute"]) & pyrogram.Filters.group)
 async def mute(bot, update):
@@ -145,14 +145,14 @@ async def mute(bot, update):
             try:
                 b = await bot.get_chat_member(update.chat.id, user_id)
             except RPCError:
-                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning")
+                await bot.send_message(chat_id=update.chat.id, text="An unknown error while banning", reply_to_message_id=update.message_id)
                 return
             mute_text = "<a href='tg://user?id={}'>{}</a> Muted <a href='tg://user?id={}'>{}</a>!".format(update.from_user.id, update.from_user.first_name, b.user.id, b.user.first_name) 
         else:
             user_id = update.reply_to_message.from_user.id
             mute_text = "<a href='tg://user?id={}'>{}</a> Muted <a href='tg://user?id={}'>{}</a>!".format(update.from_user.id, update.from_user.first_name, update.reply_to_message.from_user.id, update.reply_to_message.from_user.first_name) 
         await bot.restrict_chat_member(chat_id=update.chat.id, user_id=user_id, permissions=ChatPermissions(can_invite_users=True))
-        await bot.send_message(chat_id=update.chat.id, text=mute_text)
+        await bot.send_message(chat_id=update.chat.id, text=mute_text, reply_to_message_id=update.message_id)
     else:
         await bot.send_message(chat_id=update.chat.id, text="Who are you Non-Admin to command me?", reply_to_message_id=update.message_id)
 
