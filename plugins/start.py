34ad2ec409fa5deb2c8ebd01fa69ad65d6f5caee
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-31 18:25:27 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,6 +17,89 @@ import pyrogram
 from pyrogram import Client, Filters, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
 from database import TRChatBase
 from sql.sql import *
+from pyrogram import MessageHandler
+from concurrent.futures import Future
+import asyncio
+
+class AwaitableFuture(Future):
+    def __await__(self):
+        return (yield from asyncio.wrap_future(self))
+
+class Conversation:
+    def __init__(self, client: Client, peer):
+        self.peer = peer
+        self.client = client
+        self.handlers = []
+        self.msgs = []
+        self.message_handler = MessageHandler(self.handle_message,Filters.chat(self.peer))
+        self.last_sent_id = 0
+    def handle_message(self, _, message):
+        if not self.check(message):
+            self.msgs.append(message)
+        message.stop_propagation()
+    def add_awaiter(self, filters):
+        fut = AwaitableFuture()
+        self.handlers.append((filters,fut))
+        return fut
+    def check(self,message):
+        for filters,fut in self.handlers:
+            if fut.cancelled():
+                self.handlers.remove((filters,fut))
+            elif filters(message):
+                self.handlers.remove((filters,fut))
+                fut.set_result(message)
+                return True
+        return False
+    def send_message(self,*args, **kwargs):
+        msg = self.client.send_message(self.peer, *args, **kwargs)
+        self.last_sent_id = msg.message_id
+        return msg
+    def get_response(self,filters=Filters.create('empty',lambda *_:True)):
+        for msg in self.msgs:
+            if msg.message_id < self.last_sent_id:
+                self.msgs.remove(msg)
+            elif filters(msg):
+                fut = AwaitableFuture()
+                fut.set_result(msg)
+                self.msgs.remove(msg)
+                return fut
+        return self.add_awaiter(filters)
+    async def __aenter__(self):
+        return self.__enter__()
+    def __enter__(self):
+        self.client.add_handler(self.message_handler, -1)
+        return self
+    async def __aexit__(self):
+        self.__exit__()
+    def __exit__(self, *args):
+        self.client.remove_handler(self.message_handler, -1)
+
+class AwaitableClient(Client):
+    def conversation(self, peer):
+        return Conversation(self, peer)
+    
+    
+token = Config.TOKEN
+client = AwaitableClient('await_bot',bot_token=token,api_id=Config.APP_ID,api_hash=Config.API_HASH)
+
+@client.on_message(Filters.command('sync'))
+def _test(_, msg):
+    with bot.conversation(msg.chat.id) as conv:
+        conv.send_message('I am (sync) waiting for your reply!')
+        response = conv.get_response(Filters.text).result()
+        response.reply('You said: ' + response.text)
+
+run_async = lambda func: lambda *args: asyncio.run(func(*args))
+
+@client.on_message(Filters.command('async'))
+@run_async
+async def _test(_, msg):
+    with bot.conversation(msg.chat.id) as conv:
+        conv.send_message('I am awaiting your reply!')
+        response = await conv.get_response(Filters.text)
+        response.reply('You said: ' + response.text)
+
+bot.run()
 
 STARTKEY = [[InlineKeyboardButton("📚 Commands", callback_data="commands"), InlineKeyboardButton("ℹ️ Info", url="https://t.me/keralasbots")]]
 STARTKEY += [[InlineKeyboardButton("★ Jinja", callback_data="jinja")]]
@@ -50,9 +133,10 @@ async def jinja(bot, update):
 @Client.on_callback_query(Filters.callback_data("setjinja"))
 async def setjinja(bot, update):
     back = InlineKeyboardButton(BACKKEY)
-    await bot.edit_message_text(chat_id=update.message.chat.id, text="Now send me the jinja", message_id=update.message.message_id, reply_markup=back)
- 
-    jinja(update.from_user.id, update.message. reply_to_message.text)
-    await bot.edit_message_text(chat_id=update.message.chat.id, text="Successfully set jinja", message_id=update.message.message_id, reply_markup=back)
+    with bot.conversation(msg.chat.id) as conv:
+        await conv.send_message(chat_id=update.message.chat.id, text="Now send me the jinja", reply_markup=back)
+        response = await conv.get_response(Filters.text)
+        jinja(update.from_user.id, response.text)
+        await bot.send_message(chat_id=update.message.chat.id, text="Successfully set jinja", reply_markup=back)
 
     
