61a72c26d841538bb0a656b0803bf35613fb46c5
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-31 15:45:14 +0530

    Added SQL base

---
+++ sql/base.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import os
+DB_URI = os.environ.get("DATABASE_URL", "")
+
+
+def start() -> scoped_session:
+    engine = create_engine(DB_URI, client_encoding="utf8")
+    BASE.metadata.bind = engine
+    BASE.metadata.create_all(engine)
+    return scoped_session(sessionmaker(bind=engine, autoflush=False))
+
+
+BASE = declarative_base()
+SESSION = start()
