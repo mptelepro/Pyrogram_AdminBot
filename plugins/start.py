bd5391e29f0063370648355b49e267b63459e803
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-31 20:21:55 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -54,7 +54,7 @@ class Conversation:
         msg = self.client.send_message(self.peer, *args, **kwargs)
         self.last_sent_id = msg.message_id
         return msg
-    def get_response(self, filters=Filters.create("empty", lambda *_: True)):
+    def get_response(self, filters=Filter.text):
         for msg in self.msgs:
             if msg.message_id < self.last_sent_id:
                 self.msgs.remove(msg)
