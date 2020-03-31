a3265c967e3990bd0b6c7bb0baac90012669e40d
Developer Team[ ★彡 Anand 彡★] <62738084+Anandpskerala@users.noreply.github.com>
2020-03-31 16:29:49 +0530

    Added ping effectively

--- plugins/ping.py
+++ plugins/ping.py
@@ -7,4 +7,4 @@ async def ping(bot, update):
     first = datetime.now()
     sent = await bot.send_message(chat_id=update.chat.id, text="<B>Pong!</B>", parse_mode="html")
     second = datetime.now()
-    await bot.edit_message_text(chat_id=update.chat.id, text="**Pong!** `{(second - first).microseconds / 1000}`ms", message_id=sent.message_id)
+    await bot.edit_message_text(chat_id=update.chat.id, text="**Pong!** `{}ms`".format((second - first).microseconds / 1000), message_id=sent.message_id)
