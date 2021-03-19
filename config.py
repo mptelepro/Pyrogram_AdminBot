a7b009d8c4e9b5604876fd710ee273bb282daf55
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-19 23:33:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TOKEN = os.environ.get("BOT_TOKEN", "")
+    TOKEN = os.environ.get("1357614130:AAEnVZv3qsS1LXDBUCACJPsOSvxJgiyBiz0", "")
     # The Telegram API things
-    APP_ID = int(os.environ.get("ID", 12345))
-    API_HASH = os.environ.get("HASH")
+    APP_ID = int(os.environ.get("ID", 1749924))
+    API_HASH = os.environ.get("87e4a056bb574f7939ac283b9a9187ab")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    AUTH_USERS = set(int(x) for x in os.environ.get("984441749", "").split())
+    DB_URI = os.environ.get("postgres://eyofrnogltxmwz:9f85e8584199a374615a67bfe18f73edc4afaf9451d16eed3aee5b179708589b@ec2-54-243-195-160.compute-1.amazonaws.com:5432/d325npis91qikn", "")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
-    log_chat = -1001296501406
-    bot_name = "Edu_jokerbot"
+    log_chat = -1001367221700
+    bot_name = "Search_All_Files_bot"
     netnum = "12"
